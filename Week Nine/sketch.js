d8340fe28990a73a5cf98416344c4571cee90d43
dictatorjackson <157655355+dictatorjackson@users.noreply.github.com>
2024-03-27 16:13:00 -0600

    Add files via upload

---
+++ Week Nine/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(200,150,150);
+  rect(130,200,150,60);
+  triangle(150, 75, 200, 20, 250, 75);
+  line(280, 260, 300, 400);
+  line(100, 400, 130, 260);
+  point(200, 200);
+  point(30, 75);
+  text('Jackson Quillan', 10, 30);
+}
+
